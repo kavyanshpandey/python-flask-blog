c9099cfc064ec34ae44d8bec0e67eaa6371d2631
Apolo151 <64263339+Apolo151@users.noreply.github.com>
2021-10-29 15:57:01 +0100

    update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
+## this is a comment
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
 db = SQLAlchemy(app)
